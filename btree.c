b97c85f63c7f72af5ec1d9e56a8c9e89170c174b
diqiuzuidadezhou <1304719018@qq.com>
2017-05-25 18:18:39 +0800

    Update btree.c

--- btree.c
+++ btree.c
@@ -2,9 +2,12 @@
 typedef struct _btnode {
 	int  key;
 	int    height;
-	int parent;
 	int   lchild;
 	int   rchild;
+	union{
+	      int parent;
+	      int next;
+	};
 }btnode;
 
 void btInsert(int node);
@@ -16,12 +19,30 @@ void btAllocateNode();
 
 int m_root = NULL;
 int free_list = NULL;
-
+btmode m_nodes[];
 void btInset(int node)
 {
      if(NULL == m_root)
      {
         m_root = mode;
-	
+	m_nodes[m_root].parent = NULL;
      }
+     int data = m_nodes[node].key;
+     int index = m_root;
+     
+     while(0 != index)
+     {
+         if(data == m_nodes[index].key)
+	    return;
+	 else if(data < m_nodes[index].key)
+	 {
+	    index = m_nodes[index].lchild;
+	 }
+	 else if(data > m_nodes[index].key)	
+	 {
+	     index = m_nodes[index].rchild;
+	 }
+     }
+	
+     
 }
