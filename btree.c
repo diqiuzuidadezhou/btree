16713cf3cb3fcf88ca0c037b0a6f051759d451b8
diqiuzuidadezhou <1304719018@qq.com>
2017-05-25 16:00:15 +0800

    Update btree.c

--- btree.c
+++ btree.c
@@ -1,1 +1,14 @@
 
+typedef struct _btnode {
+	int  key;
+	int    height;
+	struct _btreenode *lchild;
+	struct _btreenode *rchild;
+}btnode;
+
+void btInsert(int node);
+void btRemove(int node);
+void btBlance(int node);
+int  btComputeHeight(int node);
+void btFreeNode(int node);
+void btAllocateNode();
